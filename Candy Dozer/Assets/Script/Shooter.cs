ca8b620921b22e4370ce0b057295d5f87430dfce
makimaki08 <08.kobayashi@gmail.com>
2023-03-04 15:25:32 +0900

    Shooter機能を改修し、Candyの種類とShoot場所を変更できるようにした

--- Candy Dozer/Assets/Script/Shooter.cs
+++ Candy Dozer/Assets/Script/Shooter.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    public GameObject candyPrefab; // Chandyプレファブ宣言
+    public GameObject[] candyPrefabs; // Chandyプレファブプロパティの配列化
+    public Transform candyParentTransform;
     public float shotForce;
     public float shotTorque;
+    public float baseWidth;
 
     // Update is called once per frame
     void Update()
@@ -14,14 +16,32 @@ public class Shooter : MonoBehaviour
         if (Input.GetButtonDown("Fire1")) Shot(); // 入力の検知
     }
 
+    // キャンディのプレハブからランダムに1つ選ぶ
+    GameObject SampleCandy()
+    {
+        int index = Random.Range(0, candyPrefabs.Length);
+        return candyPrefabs[index];
+    }
+
+    Vector3 GetInstantiatePosition()
+    {
+        // 画面のサイズとInputの割合から、キャンディ生成のポジションを計算
+        float x = baseWidth * (Input.mousePosition.x / Screen.width) - (baseWidth / 2);
+        return transform.position + new Vector3(x, 0, 0);
+    }
+
     void Shot()
     {
         // プレファブからCandyオブジェクトを生成
-        GameObject candy = Instantiate(
-            candyPrefab,
-            transform.position,
+        GameObject candy = (GameObject)Instantiate(
+            SampleCandy(),
+            GetInstantiatePosition(),
             Quaternion.identity
             );
+
+        // 生成したCandyオブジェクトの親をcandyParentTransformに設定する
+        candy.transform.parent = candyParentTransform;
+
         // CandyオブジェクトのRigidbodyを取得し、力と回転を加える
         Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
         candyRigidBody.AddForce(transform.forward * shotForce); // フォースの加算
