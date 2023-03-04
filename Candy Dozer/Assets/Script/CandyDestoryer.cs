3b274bb990eb150c2021007f10d4134465e3fccb
makimaki08 <08.kobayashi@gmail.com>
2023-03-04 16:17:55 +0900

    キャンディが落ちる場所により、ストックが回復するように改修

--- Candy Dozer/Assets/Script/CandyDestoryer.cs
+++ Candy Dozer/Assets/Script/CandyDestoryer.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class CandyDestoryer : MonoBehaviour
 {
+    public CandyManager candyManager;
+    public int reward;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Candy")
         {
+            // 指定数だけCandyのストックを増やす
+            candyManager.AddCandy(reward);
+
             // オブジェクトを削除
             Destroy(other.gameObject);
         }        
