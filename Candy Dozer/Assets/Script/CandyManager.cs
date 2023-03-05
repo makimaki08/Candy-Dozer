8d24b3e3ab7c9172c53012cdc2cf8fe7678ef967
makimaki08 <08.kobayashi@gmail.com>
2023-03-05 09:50:38 +0900

    キャンディストックの自動回復機能を追加

--- Candy Dozer/Assets/Script/CandyManager.cs
+++ Candy Dozer/Assets/Script/CandyManager.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class CandyManager : MonoBehaviour
 {
     const int DefaultCandyAmount = 30;
+    const int RecoverySeconds = 10;
 
     // 現在のキャンディのストック数
     public int candy = DefaultCandyAmount;
+    // ストック回復までの残り秒数
+    int counter;
 
     public void ConsumeCandy()
     {
@@ -30,6 +33,35 @@ public class CandyManager : MonoBehaviour
 
         // キャンディのストック数を表示
         string label = "Candy : " + candy;
-        GUI.Label(new Rect(50, 50, 100, 30), label);
+
+        // 回復カウントしている時だけ秒数を表示
+        if (counter > 0) label = label + "（" + counter + "s）";
+
+        GUI.Label(new Rect(50, 50, 150, 30), label);
+    }
+    void Update()
+    {
+        /* 
+         * キャンディのストックがデフォルトより少なく、
+         * 回復カウントをしていない時にカウントをスタートさせる
+        */
+        if(candy<DefaultCandyAmount && counter <= 0)
+        {
+            StartCoroutine(RecoverCandy());
+        }
+    }
+
+    IEnumerator RecoverCandy()
+    {
+        counter = RecoverySeconds;
+
+        // 1秒ずつカウントを進める
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1.0f);
+            counter--;
+        }
+
+        candy++;
     }
 }
